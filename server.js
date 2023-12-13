a96a4542317e9c7490c92e52f3df5ca43affcacd
Mohamed Jama <jawideggan@gmail.com>
2023-12-13 14:44:09 -0500

    changes some configuration parameters

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const jsonServer = require("json-server");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.use("/api", jsonServer.router("db.json"));
 app.use(express.static(__dirname + "/dist/truckkeeper-app"));
