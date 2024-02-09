ea4e213564552eda5ecf5ef465c029e822da54da
Mohamed Jama <jawideggan@gmail.com>
2024-02-09 09:22:22 -0500

    Added more config on heroku deploy configuration and refactored others

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
-const express = require("express");
-const jsonServer = require("json-server");
+// const express = require("express");
+// const jsonServer = require("json-server");
 
+// const app = express();
+
+// const PORT = process.env.PORT || 8000;
+
+// app.use("/api", jsonServer.router("db.json"));
+// app.use(express.static(__dirname + "/dist/truckkeeper-app"));
+
+// app.get("/*", (req, res) => {
+//   res.sendFile(__dirname + "/dist/truckkeeper-app/index.html");
+// });
+
+// app.listen(PORT, () => {
+//   console.log("Server initial port: " + PORT);
+// });
+
+// server.js
+const express = require('express');
+const path = require('path');
 const app = express();
 
-const PORT = process.env.PORT || 8000;
+app.use(express.static(__dirname + '/dist'));
 
-app.use("/api", jsonServer.router("db.json"));
-app.use(express.static(__dirname + "/dist/truckkeeper-app"));
-
-app.get("/*", (req, res) => {
-  res.sendFile(__dirname + "/dist/truckkeeper-app/index.html");
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log("Server initial port: " + PORT);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
+
